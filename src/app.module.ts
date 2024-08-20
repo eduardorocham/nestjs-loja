1b0b2d3801a33ad7e50d4db93ee919d3ff736dff
eduardo <jose.eduardo@inbec.edu.br>
2024-08-20 12:12:45 -0300

    connection with database mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ProdutoModule } from './produto/produto.module';
 import { UsuarioModule } from './usuario/usuario.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MySqlConfigService } from './config/mysql.config.service';
 
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [
+    UsuarioModule,
+    ProdutoModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: MySqlConfigService,
+      inject: [MySqlConfigService],
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/mysql.config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class MySqlConfigService implements TypeOrmOptionsFactory {
+    constructor(private configService: ConfigService) { }
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'mysql',
+            host: this.configService.get<string>('DB_HOST'),
+            port: this.configService.get<number>('DB_PORT'),
+            username: 'root',
+            password: this.configService.get<string>('DB_ROOT_PASSWORD'),
+            database: this.configService.get<string>('DB_NAME'),
+            entities: [],
+            synchronize: true,
+        };
+    }
+}
